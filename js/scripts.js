080dcca59f317fa8a06ebf8332c15a35e515af75
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-02-22 16:58:23 -0800

    finish with gender update

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,15 @@ $(document).ready(function() {
     var foodInput = parseInt($('#food').val());
     var bedtimeInput = parseInt($('#bedtime').val());
     var finalValue = parseInt(ageInput + genreInput + foodInput + bedtimeInput);
+    var sexInput = $('#sex').val();
 
-    if ((finalValue < 8) && (finalValue >= 4)) {
+    if ((finalValue <= 8) && (finalValue >= 4) && (sexInput === "male")) {
       $('#kim').show();
-    } else if (finalValue >= 8 && finalValue < 10) {
+    } else if ((finalValue >= 9) && (finalValue <= 16) && (sexInput === "male")) {
       $('#jennifer').show();
-    } else if (finalValue >= 10 && finalValue < 12) {
+    } else if ((finalValue <= 8) && (finalValue >= 4) && (sexInput === "female")) {
       $('#james').show();
-    } else if (finalValue >= 12 && finalValue <= 16) {
+    } else if ((finalValue >= 9) && (finalValue <= 16) && (sexInput === "female")) {
       $('#bruce').show();
     } else {
       alert("Please completely fill forms out");
